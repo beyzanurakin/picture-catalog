74f26bed70334b47e098c3b1c450a0567d254698
Beyzanur <akinbeyzanur@gmail.com>
2021-12-21 22:26:54 +0300

    Ejs mounted

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+app.get("/about", (req, res) => {
+  res.render('about');
+});
+
+app.get("/add_post", (req, res) => {
+  res.render('add_post');
+});
+
+app.listen(3000, () => {
+  console.log('server is up')
+})
